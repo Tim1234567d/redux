c3a340e4ca1906eae17f3e60dab6af74b922d732
temirlan <tb@plazz.ag>
2023-05-09 17:02:06 +0200

    package installed

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,11 @@ import { legacy_createStore as createStore} from 'redux'
 import { combineReducers } from 'redux'
 import { cashReducer } from './cashReducer'
 import { customerReducer } from './cutomerReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
     cash:cashReducer,
     customer:customerReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, composeWithDevTools())
